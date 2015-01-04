2242f50ca1455d8c2eabd26067dc1b1cbb30c453
Joseph White <josephbarrywhite@gmail.com>
2015-01-03 20:58:58 -0500

    clean up from previous edits

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,29 +1,21 @@
 class StoriesController < ApplicationController
   before_action :set_story, only: [:show, :edit, :update, :destroy]
 
-  # GET /stories
-  # GET /stories.json
   def index
     @stories = Story.all
   end
 
-  # GET /stories/1
-  # GET /stories/1.json
   def show
     @speeds = Speed.all
   end
 
-  # GET /stories/new
   def new
     @story = Story.new
   end
 
-  # GET /stories/1/edit
   def edit
   end
 
-  # POST /stories
-  # POST /stories.json
   def create
     @story = Story.new(story_params)
 
@@ -38,8 +30,6 @@ class StoriesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /stories/1
-  # PATCH/PUT /stories/1.json
   def update
     respond_to do |format|
       if @story.update(story_params)
@@ -52,8 +42,6 @@ class StoriesController < ApplicationController
     end
   end
 
-  # DELETE /stories/1
-  # DELETE /stories/1.json
   def destroy
     @story.destroy
     respond_to do |format|
@@ -63,12 +51,12 @@ class StoriesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_story
       @story = Story.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+
     def story_params
       params.require(:story).permit(:title, :author, :content)
     end
